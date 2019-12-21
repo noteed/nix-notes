531eec9eeb8d39b77d9d3c0ff02d3a1bcb6c715b
Vo Minh Thu <thu@hypered.io>
2019-12-21 23:54:42 +0100

    Adding a page with dependencies versions
    This commit extracts vhost.nix from configuration.nix. In the new file,
    we import site/ passing versioning information to generate a version.md
    file. Without the new file, we would have to pass all the versioning
    information every time site/ is imported.

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,6 @@
-{ config, lib, pkgs, ... }:
-let
-  gitcraft = pkgs.fetchFromGitHub {
-    owner = "noteed";
-    repo = "gitcraft";
-    rev = "96bc18ade14dca67da208b0a739f955f611cc2d5";
-    sha256 = "17lggp9kf4j288zi71pp0rqbrjzhaqx56x4hbzx8hn04m982riag";
-  };
-  git-notes = pkgs.fetchFromGitHub {
-    owner = "noteed";
-    repo = "git-notes";
-    rev = "3475e6532ec9c967584183fec7d9aafb2151bd50";
-    sha256 = "1mx297l6q353934p1rqp8yc60yjhzd6kca5pjwv9m3hq18gbny7r";
-  };
-  noteed-github-com = pkgs.fetchFromGitHub {
-    owner = "noteed";
-    repo = "noteed.github.com";
-    rev = "75fa1e634e8b4409910d83c35ddb245349985713";
-    sha256 = "1yfkwzrchc8dhzzi8pxx7rsk8l72hv84p6n40rxam5hm8x5pn22p";
-  };
-
-in
+{ config, lib, pkgs,
+  nix-notes-version,
+  ... }:
 {
   services.sshd.enable = true;
 
@@ -34,13 +15,7 @@ in
     virtualHosts."noteed.com" = {
       forceSSL = true;
       enableACME = true;
-      locations = {
-        "/add".proxyPass = "http://127.0.0.1:8000";
-        "/gitcraft/".alias = (import gitcraft {}).html.all + "/";
-        "/git-notes/".alias = (import git-notes).site + "/";
-        "/nix-notes/".alias = (import ./site {}).html.all + "/";
-        "/".alias = (import noteed-github-com {}).html.all + "/";
-      };
+      inherit (import ./vhost.nix { inherit pkgs nix-notes-version; }) locations;
     };
   };
 
--- site/default.nix
+++ site/default.nix
@@ -1,30 +1,53 @@
 { nixpkgs ? <nixpkgs>
+, nix-notes-version ? "not-given"
+, gitcraft-version ? "not-given"
+, git-notes-version ? "not-given"
+, noteed-github-com-version ? "not-given"
 }:
 
 let
   pkgs = import nixpkgs {};
 
+  design-system-version = "e1fe8d82349f4a084dee751a9c4bc5ef81ee68bb";
   design-system = pkgs.fetchFromGitHub {
     owner = "hypered";
     repo = "design-system";
-    rev = "00ec56920f1c86baa5ea0fdf96e5be79ce253fc6";
-    sha256 = "04i1hbfvsd068zgrlxgppia9ryxv2l33h00aaj9l0grkcldlkr8c";
+    rev = design-system-version;
+    sha256 = "1lqsx2zq2ymib9x4b0xncgx4wjw1mkphr4zda84fj4lbx445rdii";
   };
-  inherit (import design-system {}) to-html;
+  inherit (import design-system {}) to-html replace-md-links;
 
 in rec
 {
   md.index = ./index.md;
   md.image = ./image.md;
   md.deploying = ./deploying.md;
+  md.version = pkgs.writeText "version.md" ''
+---
+title: Versions
+footer: © Võ Minh Thu, 2019. Version ${nix-notes-version}.
+---
+
+
+----------------------------------------------------------------  ------------------------
+[design-system](https://github.com/hypered/design-system)         ${design-system-version}
+[gitcraft](https://github.com/noteed/gitcraft)                    ${gitcraft-version}
+[git-notes](https://github.com/noteed/git-notes)                  ${git-notes-version}
+[nix-notes](https://github.com/noteed/nix-notes)                  ${nix-notes-version}
+[noteed.github.com](https://github.com/noteed/noteed.github.com)  ${noteed-github-com-version}
+----------------------------------------------------------------  ------------------------
+  '';
 
   html.index = to-html md.index;
   html.image = to-html md.image;
   html.deploying = to-html md.deploying;
+  html.version = to-html md.version;
   html.all = pkgs.runCommand "all" {} ''
     mkdir $out
     cp ${html.index} $out/index.html
     cp ${html.image} $out/image.html
     cp ${html.deploying} $out/deploying.html
+    cp ${html.version} $out/version.html
+    ${pkgs.bash}/bin/bash ${replace-md-links} $out
   '';
 }
